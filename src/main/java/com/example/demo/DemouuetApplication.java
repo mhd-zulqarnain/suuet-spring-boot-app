be4616c0c249a1f8261ebe0082cbfe6a91b6b11e
xlualy <zuluparvi@gmail.com>
2018-01-07 11:08:48 +0500

    initial commit

---
+++ src/main/java/com/example/demo/DemouuetApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemouuetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemouuetApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/MyDemo.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import com.google.gson.Gson;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * oontroller
+ * data layer from user interface
+ * handle server related data,e.g http request
+ */
+@RestController ///anotate using rest controller //point sepecific poin
+@RequestMapping(path = "")
+public class MyDemo {
+
+    @RequestMapping(path = "", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    String index() {
+        return "hello";
+    }
+
+    @RequestMapping(path = "getUser", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    String getUeser() {
+        User user = new User("name", "hello", 20);
+        Gson gson = new Gson();
+
+        return gson.toJson(user);
+    }
+
+    @RequestMapping(path = "user", method = RequestMethod.GET)
+    public
+    @ResponseBody
+    String specificUser(@RequestParam(name = "id") int id) {
+        User user = new User("name", "hello", id);
+        Gson gson = new Gson();
+
+        return gson.toJson(user);
+    }
+
+
+    @RequestMapping(path = "add", method = RequestMethod.POST)
+    public @ResponseBody String adduser(@RequestParam(name = "username") String username,
+                                        @RequestParam(name = "email")String email,@RequestParam(name = "id")int id) {
+        User user = new User(username,email , id);
+        Gson gson = new Gson();
+
+        return gson.toJson(user);
+    }
+
+
+}
